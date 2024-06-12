436db17a97ef819b358803989a9b70bfad9a87a1
ostromda <olle.stromdahl@gmail.com>
2024-06-12 11:18:58 +0200

    sd

--- visualizer/script.js
+++ visualizer/script.js
@@ -1,4 +1,4 @@
-// Initialize Three.js scene, camera, and renderer V2
+// Initialize Three.js scene, camera, and renderer V3
 let scene, camera, renderer;
 
 function init() {
@@ -66,7 +66,7 @@ window.addEventListener('load', () => {
     }
 });
 
-window.onSpotifyWebPlaybackSDKReady = () => {
+function initializeSpotifyPlayer() {
     const player = new Spotify.Player({
         name: 'Web Playback SDK Template',
         getOAuthToken: cb => { cb(accessToken); }, // Provide access token
@@ -130,7 +130,7 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     });
 
     player.connect();
-};
+}
 
 function playMusic(token, device_id) {
     fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
